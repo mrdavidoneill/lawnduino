5db650b5632be7f48fe54a8cdca02722e0bbd861
mrdavidoneill <mrdavidoneillll@gmail.com>
2020-10-21 19:46:25 +0200

    Wrote full tests for Zone

--- test/test_zone/test__zone.cpp
+++ test/test_zone/test__zone.cpp
@@ -14,10 +14,45 @@ void setUp(void)
 // // clean stuff up here
 // }
 
-void test_pin_number(void)
+void testPinNumber(void)
 {
-
     TEST_ASSERT_EQUAL(5, zone.getPin());
+}
+
+void testIsOn(void)
+{
+    TEST_ASSERT_EQUAL(false, zone.getIsOn());
+    zone.turnOn();
+    TEST_ASSERT_EQUAL(true, zone.getIsOn());
+    zone.turnOff();
+    TEST_ASSERT_EQUAL(false, zone.getIsOn());
+}
+
+void testDurationMs(void)
+{
+    const unsigned long DURATION = 10000;
+    zone.setDurationMs(DURATION);
+    TEST_ASSERT_EQUAL(DURATION, zone.getDurationMs());
+    zone.setDurationMs(DURATION + DURATION);
+    TEST_ASSERT_EQUAL(DURATION + DURATION, zone.getDurationMs());
+}
+
+void testRemainingMs(void)
+{
+    zone.turnOn();
+    const unsigned long REMAINING = zone.getRemainingMs();
+    delay(1);
+    TEST_ASSERT_LESS_THAN(REMAINING, zone.getRemainingMs());
+}
+
+void testIsFinished(void)
+{
+    const unsigned long DURATION = 100;
+    zone.setDurationMs(DURATION);
+    zone.turnOn();
+    TEST_ASSERT_EQUAL(false, zone.isFinished());
+    delay(DURATION + DURATION);
+    TEST_ASSERT_EQUAL(true, zone.isFinished());
 }
 
 void setup()
@@ -27,7 +62,11 @@ void setup()
     delay(2000);
 
     UNITY_BEGIN(); // IMPORTANT LINE!
-    RUN_TEST(test_pin_number);
+    RUN_TEST(testPinNumber);
+    RUN_TEST(testIsOn);
+    RUN_TEST(testDurationMs);
+    RUN_TEST(testRemainingMs);
+    RUN_TEST(testIsFinished);
 }
 
 void loop()
