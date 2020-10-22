9b3cd9d352226aaa84570a590762184e5d8354de
mrdavidoneill <mrdavidoneillll@gmail.com>
2020-10-22 23:22:32 +0200

    First commit - Rain adjust

---
+++ lib/RainAdjust/src/RainAdjust.cpp
@@ -0,0 +1,205 @@
+#ifdef DEBUG_ESP_PORT
+#define DEBUG_MSG(...) DEBUG_ESP_PORT.printf(__VA_ARGS__)
+#else
+#define DEBUG_MSG(...)
+#endif
+
+#include "RainAdjust.h"
+
+RainAdjust::RainAdjust() {}
+
+RainAdjust::RainAdjust(WiFiClient *client, HTTPClient *http)
+{
+    _client = client;
+    _http = http;
+    DEBUG_MSG("Creating RainAdjust\n");
+    resetHourlyLog();
+    resetWeekLog();
+}
+
+RainAdjust::RainAdjust(float hourlyLog[HOURLY_LOG_LENGTH], float weekLog[WEEK_LOG_LENGTH])
+{
+    for (int i = 0; i < HOURLY_LOG_LENGTH; i++)
+    {
+        _hourlyLog[i] = hourlyLog[i];
+    }
+
+    for (int i = 0; i < WEEK_LOG_LENGTH; i++)
+    {
+        _weekLog[i] = weekLog[i];
+    }
+}
+
+void RainAdjust::resetHourlyLog()
+{
+    DEBUG_MSG("Resetting hourly log...");
+    for (int i = 0; i < HOURLY_LOG_LENGTH; i++)
+    {
+        _hourlyLog[i] = -1;
+    }
+    DEBUG_MSG("Ok\n");
+}
+
+void RainAdjust::resetWeekLog()
+{
+    DEBUG_MSG("Resetting week log...");
+    for (int i = 0; i < WEEK_LOG_LENGTH; i++)
+    {
+        _weekLog[i] = 0;
+    }
+    DEBUG_MSG("Ok\n");
+}
+
+void RainAdjust::addToHourlyLog(int mm)
+{
+    bool hasLogged = false;
+    int i = 0;
+
+    while (!hasLogged && i < HOURLY_LOG_LENGTH)
+    {
+        if (_hourlyLog[i] == -1)
+        {
+            _hourlyLog[i] = mm;
+            hasLogged = true;
+        }
+        i++;
+    }
+    if (!hasLogged)
+    {
+        resetHourlyLog();
+        addToWeekLog(sumHourlyLog());
+        _hourlyLog[0] = mm;
+    }
+}
+
+float RainAdjust::sumHourlyLog()
+{
+    float sum = 0;
+    for (int i = 0; i < HOURLY_LOG_LENGTH; i++)
+    {
+        sum += _hourlyLog[i];
+    }
+    return sum;
+}
+
+float RainAdjust::sumWeekLog()
+{
+    float sum = 0;
+    for (int i = 0; i < WEEK_LOG_LENGTH; i++)
+    {
+        sum += _weekLog[i];
+    }
+    return sum;
+}
+
+void RainAdjust::addToWeekLog(unsigned int mm)
+{
+    shiftWeekLog();
+    _weekLog[WEEK_LOG_LENGTH - 1] = mm;
+}
+
+void RainAdjust::shiftWeekLog()
+{
+    for (int i = 0; i < WEEK_LOG_LENGTH - 1; i++)
+    {
+        _weekLog[i] = _weekLog[i + 1];
+    }
+}
+
+unsigned int RainAdjust::getRainAdjust()
+{
+    unsigned int rainFall = sumWeekLog();
+    unsigned int adjust = (_targetLevel - rainFall) / _targetLevel;
+    return adjust <= 1 ? adjust : 0;
+}
+
+void RainAdjust::logRainFall()
+{
+    DEBUG_MSG("Logging rainFall\n");
+    _lastLog = millis();
+    if (fetchWeather())
+    {
+        _timeOut = 0;
+        DEBUG_MSG("Time out deactivated\n");
+    }
+    else
+    {
+        DEBUG_MSG("Time out activated\n");
+        _timeOut = TIMEOUT;
+    }
+}
+
+void RainAdjust::manage()
+{
+    if (timeToFetch())
+    {
+        logRainFall();
+    }
+}
+
+bool RainAdjust::timeToFetch()
+{
+    if (_lastLog == 0)
+    {
+        DEBUG_MSG("Time to start log...\n");
+        return true;
+    }
+
+    if (millis() > (_lastLog + _timeOut + _updateRate))
+    {
+        DEBUG_MSG("Time to log...\n");
+        return true;
+    }
+
+    return false;
+}
+
+bool RainAdjust::fetchWeather()
+{
+    DEBUG_MSG("Fetching weather\n");
+    DEBUG_MSG("Requesting weather...");
+
+    if (_http->begin(API_CALL))
+    {
+        _http->GET();
+        DEBUG_MSG("Ok\n");
+        parseWeather();
+        _http->end();
+        return true;
+    }
+    return false;
+}
+
+void RainAdjust::parseWeather()
+{
+    DEBUG_MSG("Waiting for response...\n");
+    const size_t capacity = JSON_ARRAY_SIZE(1) + 2 * JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(14) + 310;
+    DynamicJsonDocument doc(capacity * 2);
+    deserializeJson(doc, _http->getStream());
+
+    float main_temp = doc["main"]["temp"].as<float>();                     // 13.89
+    float rain_1h = doc["rain"]["1h"].as<float>();                         // 1.85
+    unsigned long sys_sunrise = doc["sys"]["sunrise"].as<unsigned long>(); // 1603262174
+    unsigned long sys_sunset = doc["sys"]["sunset"].as<unsigned long>();   // 1603301463
+    long daylightSec = sys_sunset - sys_sunrise;
+    setDaylightSec(daylightSec);
+    addToHourlyLog(rain_1h);
+    DEBUG_MSG("Rain 1h: %f\n", rain_1h);
+    DEBUG_MSG("Temp: %f\n", main_temp);
+    DEBUG_MSG("DaylightSec: %f hrs\n", (float)daylightSec / 60 / 60);
+}
+
+void RainAdjust::setDaylightSec(long daylightSec)
+{
+    _daylightSec = daylightSec;
+}
+
+unsigned int RainAdjust::getSunlightAdjust()
+{
+    // If weather not fetched from server
+    if (_daylightSec == -1)
+    {
+        return 1;
+    }
+    return (_daylightSec - MIN_SUNLIGHT_SEC) / (MAX_SUNLIGHT_SEC - MIN_SUNLIGHT_SEC);
+}
---
+++ lib/RainAdjust/src/RainAdjust.h
@@ -0,0 +1,65 @@
+/*
+  RainAdjust.h - Library for calculating rain adjust percentage.
+  Created by David A. O'Neill, October 21, 2020.
+  Released into the public domain.
+*/
+#ifndef RainAdjust_h
+#define RainAdjust_h
+
+#include <Arduino.h>
+// #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include "Secrets.h"
+
+#define DEFAULT_TARGET 50
+
+#define TIMEOUT (5 * 60 * 1000)
+#define UPDATE_RATE (60 * 60 * 1000)
+
+#define HOURLY_LOG_LENGTH 3
+#define WEEK_LOG_LENGTH (24 / 3 * 7)
+
+#define MAX_SUNLIGHT_SEC 50000
+#define MIN_SUNLIGHT_SEC 30000
+
+class RainAdjust
+{
+public:
+    RainAdjust();
+    RainAdjust(WiFiClient *client, HTTPClient *http);
+    RainAdjust(float hourlyLog[HOURLY_LOG_LENGTH], float weekLog[WEEK_LOG_LENGTH]);
+    unsigned int getRainAdjust();
+    unsigned int getSunlightAdjust();
+    void manage();
+    float sumHourlyLog();
+    float sumWeekLog();
+
+private:
+    WiFiClient *_client;
+    HTTPClient *_http;
+
+    float _hourlyLog[HOURLY_LOG_LENGTH];
+    float _weekLog[WEEK_LOG_LENGTH];
+    unsigned int _targetLevel = DEFAULT_TARGET;
+    unsigned long _lastLog = 0;
+    unsigned long _timeOut = 0;
+    unsigned long _updateRate = UPDATE_RATE;
+    long _daylightSec = -1;
+
+    void resetHourlyLog();
+    void resetWeekLog();
+    void addToHourlyLog(int mm);
+    void addToWeekLog(unsigned int mm);
+    void shiftWeekLog();
+
+    bool fetchWeather();
+    void parseWeather();
+    bool timeToFetch();
+
+    void logRainFall();
+
+    void setDaylightSec(long daylightSec);
+};
+
+#endif
