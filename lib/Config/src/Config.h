8bf777a08659900a8a94e4597a49c6eed4aa6dd0
David ONeill <mrdavidoneillll@gmail.com>
2021-02-28 10:38:05 +0100

    Add comments Move user changeable definitions to top

--- lib/Config/src/Config.h
+++ lib/Config/src/Config.h
@@ -1,23 +1,29 @@
 #ifndef Constants_h
 #define Constants_h
 
-#ifdef DEBUG_ESP_PORT
-#define DEBUG_MSG(...) DEBUG_ESP_PORT.printf(__VA_ARGS__)
-#else
-#define DEBUG_MSG(...)
-#endif
+// USER SET DEFINITIONS:
+#define NUM_OF_ZONES 6
 
+// Constants
 #define NUM_OF_WEEKDAYS 7
-#define NUM_OF_ZONES 2
+#define NUM_OF_PROGRAMS 2
 #define NUM_OF_HH_MIN 2
 
+// Time mode codes
+#define HH_OFF_CODE 24       // Off mode
+#define HH_INTERVAL_CODE 100 // Interval mode
+
+// Utility definitions
 #define MS_IN_SEC (1000)
 #define MS_IN_MIN (MS_IN_SEC * 60)
 
 #define SEC_IN_MIN (60)
 #define SEC_IN_HOUR (SEC_IN_MIN * 60)
 
+// Frequency to sync to online clock
 #define TIME_SYNC_RATE_MS (MS_IN_MIN * 3)
+
+// User timezone in seconds
 #define TZ_OFFSET_S (SEC_IN_HOUR * 2)
 
 enum WateringMode
@@ -45,5 +51,12 @@ typedef struct Status
     unsigned long timeRemainingMs;
     float weatherAdjustRate;
 } Status;
+
+// Debugger
+#ifdef DEBUG_ESP_PORT
+#define DEBUG_MSG(...) DEBUG_ESP_PORT.printf(__VA_ARGS__)
+#else
+#define DEBUG_MSG(...)
+#endif
 
 #endif
